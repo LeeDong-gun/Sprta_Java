9abde2d1a2140838758cc0f5a4bf521840a7c083
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-12 15:24:36 +0900

    study

--- src/week02/W17.java
+++ src/week02/W17.java
@@ -1,4 +1,23 @@
 package week02;
 
+import java.util.Scanner;
+
 public class W17 {
+
+    // 구구단!!!!!!!
+    public static void main(String[] args) {
+        // 입력하는 단을 제외하고 출력!!
+        Scanner sc = new Scanner(System.in);
+        int passNum = sc.nextInt(); // 출력을 제외할 구구단 수 값
+
+        for(int i = 2; i <= 9; i++) {
+            if (i != passNum) {  // != : 특정한 단 만 출력 , == : 출력을 제외할 구구단 수의 값
+                continue;
+            }
+            for(int j = 2; j <= 9; j++ ) {
+                System.out.println(i + "곱하기 " + j + "는 " + (i * j) + "입니다.");
+            }
+            System.out.println("--------------");
+        }
+    }
 }
