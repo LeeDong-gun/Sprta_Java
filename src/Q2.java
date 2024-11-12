5e63746c7b48a7a72194def25bb5f2a241f4f1ef
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-12 23:47:46 +0900

    study

--- src/Q2.java
+++ src/Q2.java
@@ -1,12 +1,66 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Q2 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String data = sc.nextLine();
 
+        String type = sc.nextLine();
+
+        Set<String> setRecipe = null;
+        List<String> listRecipe = null;
+        Map<Integer, String> mapRecipe = null;
+
+
+        if (type.equalsIgnoreCase("set")) {
+            setRecipe = new LinkedHashSet<>();
+        } else if (type.equalsIgnoreCase("list")) {
+            listRecipe = new ArrayList<>();
+        } else if (type.equalsIgnoreCase("map")) {
+            mapRecipe = new LinkedHashMap<>();
+        } else {
+            System.out.println("잘못된 구조 입력입니다.");
+        }
+
+        String title = sc.nextLine();
+
+        float rateFloat = sc.nextFloat();
+        sc.nextLine();
+
+        int rate = (int) rateFloat;
+        double ratePercent = rate * 100 / 5.0;
+
+        int step = 1;
+        while (true) {
+            String txt = sc.nextLine();
+            if (txt.equals("끝")) {
+                break;
+            } else if (setRecipe != null) {
+                setRecipe.add(txt);
+            } else if (listRecipe != null) {
+                listRecipe.add(txt);
+            } else if (mapRecipe != null) {
+                mapRecipe.put(step, txt);
+            }
+            step++;
+        }
+
+        System.out.println("[" + title + "]");
+        System.out.println("별점 :" + rate + " (" + ratePercent + "%)");
+
+        if (listRecipe != null) {
+            for (int i = 0; i <= listRecipe.size(); i++) {
+                System.out.println((i + 1) + ". " + listRecipe.get(i));
+            }
+        } else if (setRecipe != null) {
+            int j = 0;
+            for (String p : setRecipe) {
+                System.out.println((j + 1) + ". " + p);
+                j++;
+            }
+        } else if (mapRecipe != null) {
+            for (Integer key : mapRecipe.keySet()) {
+                System.out.println(key + ". " + mapRecipe.get(key));
+            }
+        }
     }
 }
