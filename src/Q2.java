516d04882dc431fda923ac4333c0705c9003d252
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-12 20:31:14 +0900

    study

---
+++ src/Q2.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String data = sc.nextLine();
+
+    }
+}
