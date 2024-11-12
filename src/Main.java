84d83b046b67455f692c42d6501395ef62702be6
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-12 09:28:01 +0900

    study

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 
 public class Main {
+
     public static void main(String[] args) {
+        // 작은 크기의 타입이 큰 크기의 타입과 '계산'될 때,
+        // 자동으로 큰 크기의 타입으로 형 변환이 됩니다.
+        int intNumber = 10;
+        double doubleNumber = 5.5;
+        double result = intNumber + doubleNumber;
 
-        //
+        System.out.println("Plus => " + result);
 
-        System.out.printf("Hello and welcome!");
+        // 1) 정수로 나누기
+        int iResult = intNumber / 4;
+        // 2) 실수로 나누기
+        double dResult = intNumber / 4.0;
+
+        System.out.println(iResult + " / " + dResult);
     }
 }
--- src/main2.java
+++ src/main2.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class main2 {
+
+    public static void main(String[] args) {
+        // 우리의 playground!
+        // 래퍼 클래스(Wrapper Class 변수)
+        int number = 21;
+
+        Integer num = number; // boxing
+
+        System.out.println(num.intValue()); // unboxing
+    }
 }
