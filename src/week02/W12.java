af29f0cdc0f7fa9139d8153b92d0443d56c896b2
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-12 12:37:26 +0900

    study

--- src/week02/W12.java
+++ src/week02/W12.java
@@ -1,4 +1,28 @@
 package week02;
 
 public class W12 {
+    public static void main(String[] args) {
+        // 중첩 if문
+        boolean flag = true;
+        int number = 2;
+
+        if (flag) {
+            if (number == 1) {
+                System.out.println("flag값은 true고, number값은 1입니다.");
+            } else if (number == 2) {
+                System.out.println("flag값은 true고, number값은 2입니다.");
+            } else {
+                System.out.println("flag값은 true고, number값은 모르겠습니다.");
+            }
+        } else {
+            System.out.println("flag가 false입니다.");
+            if (number == 1) {
+                System.out.println("flag값은 true고, number값은 1입니다.");
+            } else if (number == 2) {
+                System.out.println("flag값은 true고, number값은 2입니다.");
+            } else {
+                System.out.println("flag값은 true고, number값은 모르겠습니다.");
+            }
+        }
+    }
 }
