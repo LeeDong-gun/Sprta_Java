66322ab29c803494ca9d1c4bcb4e3752d425774f
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-13 12:22:53 +0900

    study

--- src/week03/Car.java
+++ src/week03/Car.java
@@ -13,20 +13,20 @@ public class Car {
 
     // 고유 데이터 영역
     String company; // 자동차 회사
-    String model; // 자동차 모델
+    String model = "Gv80"; // 자동차 모델
     String color; // 자동차 색
     double price; // 자동차 가격
 
     // 상태 데이터 영역
     double speed; // 자동차 속도, km/h
     char gear; // 기어상태(P, R, N, D)
-    boolean lights; // 자동차 조명의 상태
+    boolean lights = true; // 자동차 조명의 상태
 
     //객체 데이터 영역
-    Tire tire;
+    Tire tire = new Tire();
     Door door;
     Handle handle;
-    
+
 
     // <생성자 영역> : 처음 객체가 생성될 때 (istance화) 어떤 로직을 수행해야 하며, 어떤 값이 필수로 들어와야 하는지 정의!
     public Car() {
