da00c3fe22630496d056989faa2541b5a1ea9f73
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-20 09:27:27 +0900

    study

--- src/week04/gen/Generic.java
+++ src/week04/gen/Generic.java
@@ -1,11 +1,12 @@
 package week04.gen;
 
-// 1.
+// 1. 제네릭은 클래스 또는 메서드에 사용 가능!
+// <>안에 들어가야 할 타입을 명시
 public class Generic<T> {
-    // 2.
+    // 2. 내부 필드에 String
     private T t;
 
-    // 3.
+    // 3. method의 return type도 String
     public T get() {
         return this.t;
     }
